ce3dea68370d856859941501aff8d16a3105b35e
paragofficial <97843814+paragofficial@users.noreply.github.com>
2023-03-20 19:30:33 +0530

    first commit

---
+++ src/Login.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import {useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { auth, signInWithEmailAndPassword, signInWithGoogle } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "./Login.css";
+// function Login() {
+    export const Login = ()=> {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+//   const [user, loading, error] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (loading) {
+      // maybe trigger a loading screen
+      return;
+    }
+    // if (user) {
+    //   // console.log("dashboard pe ja na");
+    //   navigate("/dashboard")
+    // }
+    // if (user) navigate("/");
+    if (user) navigate("/dashboard");
+  }, [user, loading, navigate]);
+  return (
+    <div className="login">
+      <div className="login__container">
+        <input
+          type="text"
+          className="login__textBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <input
+          type="password"
+          className="login__textBox"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button
+          className="login__btn"
+          onClick={() => signInWithEmailAndPassword(auth, email, password)}
+        >
+          Login
+        </button>
+        <button className="login__btn login__google" onClick={signInWithGoogle}>
+          Login with Google
+        </button>
+        <div>
+          <Link to="/reset">Forgot Password</Link>
+        </div>
+        <div>
+          Don't have an account? <Link to="/register">Register</Link> now.
+        </div>
+      </div>
+    </div>
+  );
+}
+// export default Login;
